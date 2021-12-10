2c2e3ddbebd50df9cd776a732cc77dafb365844c
vedpatil611 <vedpatil611@gmail.com>
2021-12-10 14:37:20 +0530

    Fixed global options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,19 +1,20 @@
-local g = vim.g
+local g = vim.go
 local o = vim.o
 local b = vim.bo
 local w = vim.wo
 
-w.number         = true
-g.hlsearch       = false
-g.smartindent    = true
-b.tabstop        = 4
-b.softtabstop    = 4
-b.shiftwidth     = 4
-b.expandtab      = true
-w.wrap           = false
-g.smartcase      = true
-b.undofile       = true
-o.undodir        = '~/.vim/undodir//'
-g.directory      = '~/.vim/swapdir//'
-g.splitright     = true
-g.splitbelow     = true
+w.number        = true
+g.hlsearch      = false
+g.smartindent   = true
+b.tabstop       = 4
+b.softtabstop   = 4
+b.shiftwidth    = 4
+b.expandtab     = true
+w.wrap          = false
+g.smartcase     = true
+b.undofile      = true
+o.undodir       = '~/.vim/undodir//'
+g.directory     = '~/.vim/swapdir//'
+g.splitright    = true
+g.splitbelow	= true
+
